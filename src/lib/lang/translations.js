f9a66f3bdbcb2698991e5e0bf3824fe4f4488d8a
Tommy Hoang <tommyyhoangg@gmail.com>
2023-08-29 20:30:00 +0200

    Half working internalization

---
+++ src/lib/lang/translations.js
@@ -0,0 +1,18 @@
+export default {
+  en: {
+    nav: {
+      home: 'Home',
+      work: 'Our Work',
+      services: 'Our Services',
+      contact: 'Contact Us',
+    },
+  },
+  cz: {
+    nav: {
+      home: 'Home',
+      work: 'Our Work',
+      services: 'Our Services',
+      contact: 'Contact Us',
+    },
+  },
+};
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,129 +1,188 @@
 <script>
-	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+  import { page } from '$app/stores';
+  import logo from '$lib/images/logo.svg';
+  export let data;
+
+  $: l = data.dict;
+  $: ({ lang } = data);
+  $: menuActive = false;
+
+  // This is supposed to hit +layout.server.js but doesn't - 405 Error
+  const submitOnChange = (e) => {
+    e.target.parentElement.submit();
+  };
+
+  const menuClick = () => {
+    menuActive = !menuActive;
+  };
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
+  <div class="corner">
+    <img src={logo} alt="logo" />
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
+    <h3>Tommy Hoang</h3>
+  </div>
 
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+  <button class="menu" on:click={menuClick}>
+    <div class="menu-line" />
+  </button>
+
+  <nav class={menuActive ? 'nav-active' : undefined}>
+    <ul class={menuActive ? 'nav-content-active' : undefined}>
+      <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+        <a href="/">Home</a>
+      </li>
+      <li aria-current={$page.url.pathname === '/work' ? 'page' : undefined}>
+        <a href="/about">Our Work</a>
+      </li>
+      <li
+        aria-current={$page.url.pathname === '/services' ? 'page' : undefined}
+      >
+        <a href="/about">Services</a>
+      </li>
+      <li aria-current={$page.url.pathname === '/contact' ? 'page' : undefined}>
+        <a href="/about">Contact Us</a>
+      </li>
+    </ul>
+
+    <form method="POST" action="/language">
+      <select name="lang" bind:value={lang} on:change={submitOnChange}>
+        <option value="en">EN</option>
+        <option value="cz">CZ</option>
+      </select>
+    </form>
+  </nav>
 </header>
 
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
+<style lang="scss">
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
+    padding: 1.2em var(--padding);
+  }
 
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
+  // Nav
+  nav {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
+    width: 0;
+    height: 0;
+    top: -6.5rem;
+    right: -4rem;
+    position: fixed;
+    padding: 0 0 6.4rem 2.5rem;
+    z-index: 500;
 
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
+    background: var(--accent);
+    border-radius: 50%;
+    transition: all 400ms;
 
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
+    ul {
+      margin: 6rem 0 0 0;
+      opacity: 0;
 
-	path {
-		fill: var(--background);
-	}
+      transition: 300ms ease-in;
+      transition-delay: -0.5s;
 
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
+      li {
+        margin: 0.67rem 0 0 0;
 
-	li {
-		position: relative;
-		height: 100%;
-	}
+        font-size: 1.82rem;
+        font-weight: 600;
+        list-style-type: none;
+      }
 
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
+      a {
+        color: var(--primary);
+        text-decoration: none;
+      }
+    }
+  }
 
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
+  // Nav - content animation
+  .nav-active {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 0;
 
-	a:hover {
-		color: var(--color-theme-1);
-	}
+    border-radius: 0;
+  }
+
+  .nav-content-active {
+    opacity: 100%;
+
+    transition: 300ms ease-in;
+    transition-delay: 0.5s;
+  }
+
+  // Left corner
+  .corner {
+    display: flex;
+    align-items: center;
+
+    // Logo
+    img {
+      width: 2.64em;
+    }
+
+    // Title
+    h3 {
+      margin: 0 0 0 0.42em;
+      padding: 0.77em 1.7em;
+
+      font-size: 0.72em;
+      font-weight: 600;
+      color: var(--nav-title);
+      background: var(--primary2);
+      border-radius: 5em;
+    }
+  }
+
+  // Menu icon
+  .menu {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 2.7em;
+    height: 2.7em;
+    right: var(--padding);
+    z-index: 550;
+    position: fixed;
+
+    border-radius: 50%;
+    box-shadow: 2px 2px 30px rgba(0, 0, 0, 0.3);
+    background: var(--primary);
+    border: none;
+  }
+
+  .menu-line {
+    width: 1.2em;
+    height: 2px;
+    position: relative;
+
+    background: var(--second);
+
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      width: 1.2em;
+      height: 2px;
+      transform: translate(50%, -5px);
+      right: 50%;
+
+      background: var(--second);
+    }
+
+    &::after {
+      transform: translate(50%, 5px);
+    }
+  }
 </style>
