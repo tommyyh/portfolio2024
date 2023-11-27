a735b5eff7c67e214877daac68702440df1e13d6
Tommy Hoang <tommyyhoangg@gmail.com>
2023-11-27 21:11:34 +0100

    Scroll up function

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,15 +1,45 @@
 <script>
   import logo from '$lib/images/logo.svg';
+  import chevronDownSvg from '$lib/images/chevron-down.svg';
   import Navigation from '../components/Header/Navigation.svelte';
   import DesktopNavigation from '../components/Header/DesktopNavigation.svelte';
+  import { onMount } from 'svelte';
   export let t;
 
   $: menuActive = false;
+  $: upActive = false;
 
   // Open/Close menu
   const menuClick = () => {
     menuActive = !menuActive;
   };
+
+  // Scroll up
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  onMount(() => {
+    const scrollFn = () => {
+      const topDistance = window.scrollY;
+
+      if (topDistance > 700) {
+        upActive = true;
+      } else {
+        upActive = false;
+      }
+    };
+
+    window.addEventListener('scroll', scrollFn);
+
+    return () => {
+      window.removeEventListener('scroll', scrollFn);
+    };
+  });
 </script>
 
 <div class="cont">
@@ -26,6 +56,10 @@
 
     <Navigation {t} {menuActive} />
     <DesktopNavigation {t} />
+
+    <div class={upActive ? 'up up-active' : 'up'} on:click={scrollUp}>
+      <img src={chevronDownSvg} alt="chevron down" />
+    </div>
   </header>
 </div>
 
@@ -105,6 +139,37 @@
     }
   }
 
+  // Up thing
+  .up {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 2.5rem;
+    height: 2.5rem;
+    position: fixed;
+    bottom: -2.5rem;
+    right: 1rem;
+    z-index: 400;
+
+    background: var(--primary);
+    border-radius: 50%;
+    box-shadow: 0px 4px 73px 0px rgba(0, 0, 0, 0.7);
+    transition: 230ms ease-in-out;
+    opacity: 0;
+
+    img {
+      filter: brightness(0.15);
+      width: 0.78rem;
+      transform: rotate(180deg);
+    }
+  }
+
+  .up-active {
+    bottom: 1rem;
+    opacity: initial;
+  }
+
   @media all and (min-width: 1025px) {
     .cont {
       display: flex;
@@ -151,6 +216,43 @@
     // Menu icon
     .menu {
       display: none;
+    }
+
+    // Up thing
+    .up {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      width: 2.8rem;
+      height: 2.8rem;
+      position: fixed;
+      bottom: -2.7rem;
+      right: 2.7rem;
+      z-index: 400;
+
+      cursor: pointer;
+      background: var(--primary);
+      border-radius: 50%;
+      box-shadow: 0px 4px 73px 0px rgba(0, 0, 0, 0.99);
+      transition: 300ms ease;
+      border: 0px solid #fff;
+
+      img {
+        filter: brightness(0.15);
+        width: 0.78rem;
+        transform: rotate(180deg);
+      }
+
+      &:hover {
+        background: var(--primary3);
+        box-shadow: 0px 5px 50px 0px rgba(0, 0, 0, 1);
+      }
+    }
+
+    .up-active {
+      bottom: 2.7rem;
+      opacity: initial;
     }
   }
 </style>
